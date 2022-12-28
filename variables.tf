a70d8e213d585d365d26d3f913a474853d9a4809
John Fabry <jjfabry@pm.me>
2022-12-28 09:31:27 -0500

    Adds new variable.

--- variables.tf
+++ variables.tf
@@ -6,6 +6,10 @@ variable "instance_type-2" {
  type        = string
  description = "EC2 instance type"
 }
+variable "instance_type-4" {
+ type        = string
+ description = "EC2 instance type"
+}
 variable "ami-1" {
  type        = string
  description = "AMI for workload"
